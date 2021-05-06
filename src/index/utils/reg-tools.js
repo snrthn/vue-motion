0a2905cece430258fbf2b3deaa94d3523f29321c
杨虎宁 <yanghuning@imeik.com>
2021-05-06 23:13:25 +0800

    '自动工具修复'

--- src/index/utils/reg-tools.js
+++ src/index/utils/reg-tools.js
@@ -1,29 +1,36 @@
 /**
- * @files 将 @/utils/global、@/api/module 目录下的方法自动注册到全局
+ * @files 将 @/utils/global 和 @/api/modules 目录下的方法自动注册到全局
  * @author yanghuning
  */
 
-function install(Vue) {
-  var reFun1 = require.context('@/api/modules', true, /\.js$/i);
-  var reFun2 = require.context('@/utils/global', true, /\.js$/i);
-  var reArr = [...reFun1.keys(), ...reFun2.keys()];
-  reArr.map(function(item) {
-    if (item === './index.js') return;
-    var toolObj = (function () {
-      try {
-        return reFun1(item);
-      } catch (e) {
-        return reFun2(item);
+ function install(Vue) {
+  // 注册 @/utils/global 目录
+  var reFun1 = require.context('@/utils/global', true, /\.js$/i)
+  var reArr2 = reFun1.keys()
+  reArr2.map(function(item) {
+    var toolObj1 = reFun1(item)
+    if (typeof toolObj1 === 'function') {
+      Vue.prototype[toolObj1.name] = toolObj1
+    } else if (typeof toolObj1 === 'object') {
+      for (var key in toolObj1) {
+        Vue.prototype[key] = toolObj1[key]
       }
-    }());
-    if (typeof toolObj === 'function') {
-      Vue.prototype[toolObj.name] = toolObj;
-    } else if (typeof toolObj === 'object') {
-      for (var key in toolObj) {
-        Vue.prototype[toolObj[key].name] = toolObj[key];
+    }
+  })
+
+  // 注册 @/api/modules 目录
+  var reFun2 = require.context('@/api/modules', true, /\.js$/i)
+  var reArr2 = reFun2.keys()
+  reArr2.map(function(item) {
+    var toolObj2 = reFun2(item)
+    if (typeof toolObj2 === 'function') {
+      Vue.prototype[toolObj2.name] = toolObj2
+    } else if (typeof toolObj2 === 'object') {
+      for (var key in toolObj2) {
+        Vue.prototype[key] = toolObj2[key]
       }
     }
   })
 }
 
-export default { install };
+export default { install }
